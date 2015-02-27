5fce99d0108a0affbc8b0776ae399195a9b09123
aleXpansion <alxpansion@gmail.com>
2015-02-26 20:53:25 -0700

    Getting stuff set up

---
+++ src/main/java/com/alexpander/testmod/proxy/ServerProxy.java
@@ -0,0 +1,7 @@
+package com.alexpander.testmod.proxy;
+
+/**
+ * Created by Danny on 2/26/2015.
+ */
+public class ServerProxy extends CommonProxy {
+}
---
+++ src/main/java/com/alexpander/testmod/reference/Reference.java
@@ -0,0 +1,12 @@
+package com.alexpander.testmod.reference;
+
+/**
+ * Created by Danny on 2/26/2015.
+ */
+public class Reference {
+    public static final String MOD_ID = "TestMod";
+    public static final String MOD_NAME = "TestMod";
+    public static final String VERSION = "1.7.10-0.1";
+    public static final String CLIENT_PROXY_CLASS = "com.alexpander.testmod.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.alexpander.testmod.proxy.ServerProxy";
+}
